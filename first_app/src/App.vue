300e2be99bbf33468b1a45622d7158861700bf82
stanislaw8 <pavlovinthesky@gmail.com>
2021-05-31 06:35:05 +0300

    lesson5 first app

--- first_app/src/App.vue
+++ first_app/src/App.vue
@@ -1,26 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Hello msg="Welcome to Yo3223ur Vue.js App"/>
+  <Data/>
+    <div>footer</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Hello from './components/HelloWorld.vue'
+import Data from './components/someData'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hello,
+    Data,
   }
 }
 </script>
 
 <style>
 #app {
+    background-color: red;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.hello {
+    background-color: royalblue;
 }
 </style>
